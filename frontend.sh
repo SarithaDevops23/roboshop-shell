cd1c02c7199ab1433b9fa9cdea41052fc9d523f0
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-27 06:44:10 +0530

    print command messages

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,12 @@
 echo -e "\e[33mInstalling nginx server\e[0m"
 yum install nginx -y 
+
+echo -e "\e[33mRemoving static content from server\e[0m"
 rm -rf /usr/share/nginx/html/* 
+
+echo -e "\e[33mdownloading actual content from the project\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "\e[33munzipping downloaded content\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
