a3901ea64f6bee64d0fa4d94f92ccec9fccb26fe
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-27 13:50:35 +0530

    mondb redirectors added

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,8 @@
 echo -e "\e[34mcopying repo file to yum dir\e[0m"
 cp mongodb.repo /etc/yum.repos.d
 echo -e "\e[34minstalling mongo db org\e[0m"
-yum install mongodb-org -y 
+yum install mongodb-org -y &>>/tmp/mongodb.log
 ## need to change default ip to 0.0.0.0
 systemctl enable mongod 
-systemctl restart mongod 
+systemctl restart mongod  &>>/tmp/mongodb.log
 echo -e "\e[34mmongo db started\e[0m"
