a58cd1016000e0fc0614f4dc67d9d8a2a476311f
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-27 06:40:39 +0530

    printing messages

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+echo -e "\e[33mInstalling nginx server\e[0m"
 yum install nginx -y 
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
@@ -6,3 +7,4 @@ unzip /tmp/frontend.zip
 
 systemctl enable nginx 
 systemctl restart nginx 
+echo -e "\e[33minstallation done\e[0m"
