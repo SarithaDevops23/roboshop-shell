bc8947bfb565837f2bbc121267ec7026db8c208f
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-28 16:35:15 +0530

    .service file paths updated while copying

--- catalogue.sh
+++ catalogue.sh
@@ -21,7 +21,7 @@ echo -e "\e[33m installing nodejs dependencies npm \e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33m setup catalogue.servive into systemd directory \e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[33m daemon reload\e[0m"
 systemctl daemon-reload
@@ -30,7 +30,7 @@ echo -e "\e[33m restarting catalogue\e[0m"
 systemctl restart catalogue &>>/tmp/roboshop.log
 
 echo -e "\e[33m copying mongodb repo in order to load schema we need this\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo 
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo 
 echo -e "\e[33m installing mongodb client \e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
