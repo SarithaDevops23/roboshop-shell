cfbea34478ba380dcb8fed0313bd07ccf20d21c3
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-28 16:40:45 +0530

    sed -e error in mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -4,9 +4,9 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[33minstalling mongo db org\e[0m"
 yum install mongodb-org -y &>>/tmp/mongodb.log
 ## need to change default ip to 0.0.0.0
-echo -e "\e[33m replacing mongod locahost to 0.0.0.0\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+echo -e "\e[33mreplacing mongod locahost to 0.0.0.0\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 
 systemctl enable mongod 
 systemctl restart mongod  &>>/tmp/mongodb.log
-echo -e "\e[34mmongo db started\e[0m"
+echo -e "\e[33mmongo db started\e[0m"
