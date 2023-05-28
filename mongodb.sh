26e7848ce9dbb1f0273e1ed7db2884cfb7ff9d13
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-28 16:42:58 +0530

    sed -e

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[33minstalling mongo db org\e[0m"
 yum install mongodb-org -y &>>/tmp/mongodb.log
 ## need to change default ip to 0.0.0.0
 echo -e "\e[33mreplacing mongod locahost to 0.0.0.0\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod 
 systemctl restart mongod  &>>/tmp/mongodb.log
