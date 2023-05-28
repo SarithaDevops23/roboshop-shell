3b114cc5788870057a48a34eadfb2239e688c784
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-28 15:44:44 +0530

    sed editor substitute function for replacing localhost address to 0.0.0.0

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,11 @@
-echo -e "\e[34mcopying repo file to yum dir\e[0m"
+echo -e "\e[33mcopying repo file to yum dir\e[0m"
 cp mongodb.repo /etc/yum.repos.d
-echo -e "\e[34minstalling mongo db org\e[0m"
+echo -e "\e[33minstalling mongo db org\e[0m"
 yum install mongodb-org -y &>>/tmp/mongodb.log
 ## need to change default ip to 0.0.0.0
+echo -e "\e[33m replacing mongod locahost to 0.0.0.0\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
 systemctl enable mongod 
 systemctl restart mongod  &>>/tmp/mongodb.log
 echo -e "\e[34mmongo db started\e[0m"
