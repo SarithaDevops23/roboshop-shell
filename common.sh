013ed981f2eaade5e0b2e3279583bf9fceef46a8
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-05-30 00:10:09 +0530

    log syntax correction

--- common.sh
+++ common.sh
@@ -1,13 +1,13 @@
-output_log="&>>/tmp/roboshop.log"
+output_log="/tmp/roboshop.log"
 color="\e[33m"
 nocolor="\e[0m"
 
 nodejs(){
-	echo -e "\e[33mconfiguring nodejs repos${nocolor}"
-    curl -sL https://rpm.nodesource.com/setup_lts.x | bash $output_log
+	echo -e "${color}configuring nodejs repos${nocolor}"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$output_log
 
-	echo -e "\e[33minstalling nodejs${nocolor}"
-	yum install nodejs -y $output_log
+	echo -e "${color}installing nodejs${nocolor}"
+	yum install nodejs -y &>>$output_log
 
 	echo -e "${color} user adding ${nocolor}"
 	useradd roboshop
@@ -17,21 +17,21 @@ nodejs(){
 	mkdir /app 
 
 	echo -e "${color} downloading catalogue content ${nocolor}"
-	curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip $output_log
+	curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$output_log
 	cd /app 
 	echo -e "${color} extracting downloaded content in application dirctory${nocolor}"
-	unzip /tmp/$component.zip $output_log
+	unzip /tmp/$component.zip &>>$output_log
 	cd /app 
 	echo -e "${color} installing nodejs dependencies npm ${nocolor}"
-	npm install $output_log
+	npm install &>>$output_log
 
 	echo -e "${color} setup catalogue.servive into systemd directory ${nocolor}"
-	cp /root/roboshop-shell/$component.service /etc/systemd/system/$component.service $output_log
+	cp /root/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$output_log
 
 	echo -e "${color} copying mongodb repo in order to load schema we need this${nocolor}"
 	cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo 
 	echo -e "${color} installing mongodb client ${nocolor}"
-	yum install mongodb-org-shell -y $output_log
+	yum install mongodb-org-shell -y &>>output_log
 
 	echo -e "${color} loading schema${nocolor}"
 	mongo --host mongodb-dev.devops23.store </app/schema/$component.js $output_log
--- frontend.sh
+++ frontend.sh
@@ -2,21 +2,21 @@ source common.sh
 
 
 echo -e "${color}Installing nginx server${nocolor}"
-yum install nginx -y $outpit_log
+yum install nginx -y &>>$output_log
 
 echo -e "${color}Removing static content from server${nocolor}"
-rm -rf /usr/share/nginx/html/* $outpit_log
+rm -rf /usr/share/nginx/html/* &>>$output_log
 
 echo -e "${color}downloading actual content from the project${nocolor}"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip $outpit_log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$output_log
 echo -e "${color}unzipping downloaded content${nocolor}"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip $outpit_log
+unzip /tmp/frontend.zip &>>$output_log
 
 echo -e "${color}update frontend configuration${nocolor}"
 cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
-systemctl enable nginx $outpit_log
-systemctl restart nginx $outpit_log
+systemctl enable nginx &>>$output_log
+systemctl restart nginx &>>$output_log
 echo -e "${color}installation done${nocolor}"
